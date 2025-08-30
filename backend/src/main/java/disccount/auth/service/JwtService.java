a42d64f794cfde2cd043306cb374224a97c369db
CrazyFreak <jakovbla4@gmail.com>
2025-08-30 18:30:18 +0200

    remove unused import for UserDetails in JwtService

--- backend/src/main/java/disccount/auth/service/JwtService.java
+++ backend/src/main/java/disccount/auth/service/JwtService.java
@@ -3,7 +3,6 @@ package disccount.auth.service;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
