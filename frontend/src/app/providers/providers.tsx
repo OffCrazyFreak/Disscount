762b1f7a1860a534b88a4f74c77422cde40adfb8
CrazyFreak <jakovbla4@gmail.com>
2025-08-30 15:45:19 +0200

    Added react scan

--- frontend/src/app/providers/providers.tsx
+++ frontend/src/app/providers/providers.tsx
@@ -5,13 +5,18 @@ import { ReactQueryProviderWrapper } from "@/app/providers/react-query-provider"
 import { SidebarProvider } from "@/app/providers/sidebar-provider";
 import { ToasterProvider } from "@/app/providers/toaster-provider";
 import { UserContextProvider } from "@/app/providers/user-provider";
+import { ReactScan } from "./react-scan";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ReactQueryProviderWrapper>
       <UserContextProvider>
         <SidebarProvider>
-          <ToasterProvider>{children}</ToasterProvider>
+          <ToasterProvider>
+            <ReactScan />
+
+            {children}
+          </ToasterProvider>
         </SidebarProvider>
       </UserContextProvider>
     </ReactQueryProviderWrapper>
